08c501c4c0d37953e17faa936a1183a802ec7e91
Suncraft <cookies666@msn.com>
2021-04-08 15:12:49 +0000

    starting ex2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,7 +9,7 @@ puts "----------"
 
 # end
 
-urnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,25 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id:1)
+puts @store1.name
+
+
+
+
+
+# Exercise 2: Update the first store
+
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+
+
+# CREATE TABLE "stores" ("id" serial primary key,
+# "name" character varying,
+# "annual_revenue" integer,
+# "mens_apparel" boolean,
+# "womens_apparel" boolean,
+# "created_at" timestamp NOT NULL,
+# "updated_at" timestamp NOT NULL)
